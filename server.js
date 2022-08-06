521624d4376c77cbafffa4a62f6199968aae081c
Surajbnp <97525465+Surajbnp@users.noreply.github.com>
2022-08-06 14:20:06 +0530

    changes

---
+++ server.js
@@ -0,0 +1,19 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("/db.json");
+const middlewares = jsonServer.defaults({
+  static: "./build",
+});
+
+const port = process.env.PORT || 8080;
+server.use(middlewares);
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+  })
+);
+
+server.use(router);
+server.listen(port, () => {
+  console.log(`server is running on ${port}`);
+});
