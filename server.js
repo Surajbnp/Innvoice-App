94967be6cc8ae825d47018043421223132d67eaf
Surajbnp <97525465+Surajbnp@users.noreply.github.com>
2022-08-07 02:36:19 +0530

    json

--- server.js
+++ server.js
@@ -1,18 +1,12 @@
-const jsonServer = require('json-server');
+const data = require("./db.json");
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const app = jsonServer.create();
-const path = require('path');
-const express = require('express');
+const router = jsonServer.rewriter(data);
 const middlewares = jsonServer.defaults();
-const router = jsonServer.router('db.json');
-const port = process.env.PORT || 8080;
 
-app.use('/db', middlewares, router);
-app.use(express.static(path.join(__dirname, 'build')));
+const port = process.env.PORT || 8080
 
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+server.use(middlewares);
+server.use(router)
 
 server.listen(port);
-
