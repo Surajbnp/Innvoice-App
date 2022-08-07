9a9db529f63846feb238f1db53555d08d85c4c70
Surajbnp <97525465+Surajbnp@users.noreply.github.com>
2022-08-07 15:40:33 +0530

    done

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const middlewares = jsonServer.defaults({
     static : './build'
 })
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 server.use(middlewares)
 server.use(jsonServer.rewriter({
     '/api/*' : '/$1',
