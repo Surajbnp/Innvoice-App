59e2bdc93411425f1f21fea065c6f89b210c5b36
Surajbnp <97525465+Surajbnp@users.noreply.github.com>
2022-08-07 14:26:50 +0530

    chnages

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
-const data = require("./db.json");
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.rewriter(data);
-const middlewares = jsonServer.defaults();
+const router = jsonServer.router("./db.json");
+const middlewares = jsonServer.defaults({
+  static: "./build",
+});
 
-const port = process.env.PORT || 3000
-
+const PORT = process.env.PORT || 8080;
 server.use(middlewares);
-server.use(router)
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+  })
+);
 
-server.listen(port);
+server.use(router);
+server.listen(PORT, () => {
+  console.log("Server is running ");
+});
+
